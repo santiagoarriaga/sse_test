5db1b5ff98aecc9783b9ef5902f7b548994b35f2
santiagonoguez <santiago.arriaga9@gmail.com>
2020-08-27 10:19:40 -0500

    leer de ambiente

--- src/main/java/test/Config.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-public interface Config
-{
-
-  String BOOTSTRAP_SERVERS = "localhost:9092";
-  String TOPIC = "test-events";
-
-}
--- src/main/java/test/services/Services.java
+++ src/main/java/test/services/Services.java
@@ -5,48 +5,54 @@ import java.util.*;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import reactor.core.publisher.Flux;
 
-import test.Config;
 import test.data.IncomingMessage;
 
 @Configuration
 public class Services
 {
 
-  Map<String, Object> CONSUMER_PROPERTIES = Collections.unmodifiableMap
-  (
-    new HashMap<String, Object>()
-    {{
-      put( ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, Config.BOOTSTRAP_SERVERS );
-      put( ConsumerConfig.CLIENT_ID_CONFIG        , "sample_receiver" );
-    }}
-  );
-
-  Map<String, Object> PRODUCER_PROPERTIES = Collections.unmodifiableMap
-  (
-    new HashMap<String, Object>()
-    {{
-      put( ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, Config.BOOTSTRAP_SERVERS );
-      put( ProducerConfig.CLIENT_ID_CONFIG        , "sample_sender"   );
-    }}
-  );
-
   private Flux<IncomingMessage> _liveFlux;
+
+  @Value( "${APP_BOOTSTRAP_SERVERS}" )
+  private String bootstrapServers;
+
+  @Value( "${APP_TOPIC}" )
+  private String topic;
 
   @Bean
   public KafkaMessageProducer kafkaMessageProducer()
   {
-    return new KafkaMessageProducer( PRODUCER_PROPERTIES, Config.TOPIC );
+    Map<String, Object> PRODUCER_PROPERTIES = Collections.unmodifiableMap
+    (
+      new HashMap<String, Object>()
+      {{
+        put( ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers );
+        put( ProducerConfig.CLIENT_ID_CONFIG        , "sample_sender"  );
+      }}
+    );
+
+    return new KafkaMessageProducer( PRODUCER_PROPERTIES, topic );
   }
 
   @Bean
   public KafkaMessageSource kafkaMessageSource()
   {
-    return new KafkaMessageSource( CONSUMER_PROPERTIES, Config.TOPIC );
+    var CONSUMER_PROPERTIES = Collections.unmodifiableMap
+    (
+      new HashMap<String, Object>()
+      {{
+        put( ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers  );
+        put( ConsumerConfig.CLIENT_ID_CONFIG        , "sample_receiver" );
+      }}
+    );
+
+    return new KafkaMessageSource( CONSUMER_PROPERTIES, topic );
   }
 
   @Bean Flux<IncomingMessage> flow( @Autowired KafkaMessageSource source )  
